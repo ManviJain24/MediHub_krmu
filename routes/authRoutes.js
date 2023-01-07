62884ab5ce79fe045b2a20264bf596f0c4cb9e8e
SMITA TRIPATHI <smita@meetu2111@gmail.com>
2023-01-07 23:52:20 +0530

    some added to authentication page

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const authController = require('../controllers/authController');
+
+// const getAllMember=require("../controllers/api")
+const router = Router();
+
+router.get('/signup', authController.signup_get);
+router.post('/signup', authController.signup_post);
+router.get('/login', authController.login_get);
+router.post('/login', authController.login_post);
+router.get('/logout', authController.logout_get);
+router.get('/data',)
+
+module.exports = router;
